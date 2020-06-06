4c943849af75408c5aac4487a8dae0be7e1b54ae
nguyen dinh doanh <nguyendinhdoanhsn99@gmail.com>
2020-06-06 22:03:18 +0700

    Day1

---
+++ BaiTapVeNhaBuoi1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BaiTapVeNhaBuoi1
+//
+//  Created by Đình Doanh on 6/6/20.
+//  Copyright © 2020 Doanh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Day1
+- Bài Tập về Nhà Day 1
